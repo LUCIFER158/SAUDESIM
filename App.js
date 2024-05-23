ba4d863bd1fa086a3936f8857085edd1e1742180
LUCIFER158 <tierriwalacee@gmail.com>
2024-05-23 08:49:22 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+import Title from './SRC/components/Title';
+import Main from './SRC/components/Main';
+export default function App() {
+  return (
+    <View style={styles.container}>
+     <Title/> 
+     <Main/>    
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
